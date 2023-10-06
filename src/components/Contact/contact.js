80478fab94afa827b13e8621c91143401426f32d
AGHOSH B PRASAD <aghoshbprasad100@gmail.com>
2023-10-06 09:11:01 +0530

    Added components

---
+++ src/components/Contact/contact.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import walmart from '../../assets/walmart.png';
+import adobe from '../../assets/adobe.png';
+import microsoft from '../../assets/microsoft.png';
+import facebook from '../../assets/facebook.png';
+import facebookIcon from '../../assets/facebook-icon.png';
+import twitterIcon from '../../assets/twitter.png';
+import youtubeIcon from '../../assets/youtube.png';
+import instagramIcon from '../../assets/instagram.png';
+import './contact.css';
+
+const Contact = () => {
+  return (
+    <section id="contactPage">
+        <div id="clients">
+            <h1 className="contactPageTitle">My clients</h1>
+            <p className="clientDesc">I have had the opportunity to work with a diverse group of companies. Some of the notable companies I have worked with includes</p>
+            <div className="clientImgs">
+                <img src={walmart} alt="" className="clientImg" />
+                <img src={adobe} alt="" className="clientImg" />
+                <img src={microsoft} alt="" className="clientImg" />
+                <img src={facebook} alt="" className="clientImg" />
+            </div>
+        </div>
+
+        <div id="contact">
+            <h1 className="contactPageTitle">Contact Me</h1>
+            <span className="contactDesc">Fill out the form below to discuss any work opportunities,</span>
+            <form action="" className="contactForm">
+                <input type="text" className="name" placeholder='Your Name'/>
+                <input type="email" className="email" placeholder='Your Email'/>
+                <textarea name="message" className='message' id="" rows="5" placeholder='Your Message'></textarea>
+                <button type='submit' value='send' className='submitBtn' >Submit</button>
+                <div className="links">
+                    <img src={facebookIcon} alt="facebook" className="link" />
+                    <img src={twitterIcon} alt="twitter" className="link" />
+                    <img src={youtubeIcon} alt="youtube" className="link" />
+                    <img src={instagramIcon} alt="instagram" className="link" />
+                </div>
+            </form>
+        </div>
+    </section>
+  )
+}
+
+export default Contact
---
+++ src/components/Intro/intro.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./intro.css";
+import bg from "../../assets/image.png";
+import btnImg from "../../assets/hireme.png";
+import { Link } from "react-scroll";
+
+const Intro = () => {
+  return (
+    <section id="intro">
+      <div className="introContent">
+        <span className="hello">Hello, </span>
+        <span className="introText">
+          I'm <span className="introName">Smith</span>
+          <br />
+          Website Designer{" "}
+        </span>
+        <p className="introPara">
+          I am a skilled web designer with experience in creating <br />{" "}
+          visually appealing and user friendly websites.
+        </p>
+        <Link>
+          <button className="btn">
+            <img src={btnImg} alt="Hire me" className="btnImg" />
+            Hire me
+          </button>
+        </Link>
+      </div>
+        <img className="bg" src={bg} alt="profile" />{" "}
+    </section>
+  );
+};
+
+export default Intro;
